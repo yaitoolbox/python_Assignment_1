98a08cd4dd556df3cd0840cad83222f75343d577
yaitoolbox <168286421+yaitoolbox@users.noreply.github.com>
2025-02-25 13:13:50 +0530

    Add files via upload

---
+++ Assignment_1_Task_1.py
@@ -0,0 +1,30 @@
+
+# Basic Mathematical Calcualtions
+
+# User input
+
+number1 = int(input("Enter the first number : "))
+number2 = int(input("Enter the second number : "))
+
+#Displaying all the operations on the screen
+
+print("Addition : ", (number1 + number2))
+print("Subtraction :", (number1 - number2))
+print("Multiplication :", (number1 * number2))
+print("Division :", (number1/number2))
+
+'''
+#assigning the variable for operation
+operation = input("Enter the Operator +, -, * or / : ")
+
+# Conditional Statement
+
+if operation == "+":
+    print("Addition ::", number1 + number2)
+elif operation == "-" :
+    print("Subtraction ::", number1 - number2)
+elif operation == "*":
+    print("Multiplication :: ", number1 * number2)
+else:
+    print("Division :: ", number1 /number2)
+'''
---
+++ Assignment_1_Task_2.py
@@ -0,0 +1,11 @@
+
+# Task 2: Create a Personalized Greeting
+
+# User Input
+
+first_name = input("Enter First Name : ")
+last_name = input("Enter Last Name : ")
+
+#Displaying the output
+
+print(f"Hello, {first_name +" "+ last_name}! Welcome to the Python Program.")
